7711f125829b9560d57987fc0f910b2afc355328
stellakang <kangsj123@naver.com>
2023-02-08 13:38:14 +0900

    Update comment

--- src/Structure.java
+++ src/Structure.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class Structure {
     public List<Long> getPrimeList(String data) {
-        // data form: "1.2.3.4.5"
+        // data form: "1|2|3|4|5"
         List<String> elements = Arrays.asList(data.split("\\|"));
         List<Long> numbers = new ArrayList<Long>();
         for (String str : elements) numbers.add(Long.valueOf(str));
@@ -17,7 +17,7 @@ public class Structure {
         String result = numbers.stream()
                 .map(n -> String.valueOf(n))
                 .collect(Collectors.joining("|"));
-        // return: String type (ex. "1.2.3.4.5")
+        // return: String type (ex. "1|2|3|4|5")
         return result;
     }
 }
