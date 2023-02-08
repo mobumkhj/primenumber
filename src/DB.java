dee762d3a621ae36e624726879700229b3345ad7
mobum <mobumkhj@gmail.com>
2023-02-08 13:23:17 +0900

    Add DB
    Signed-off-by: mobum <mobumkhj@gmail.com>

--- src/DB.java
+++ src/DB.java
@@ -1,10 +1,56 @@
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DB {
+
     public List getPrimeNumbers(Long number) {
-        return null;
+        BufferedReader bufferedReader = null;
+        try {
+            List<Integer> list = new ArrayList<>();
+
+            bufferedReader = new BufferedReader(new FileReader("db.txt"));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+
+                line = line.trim();
+                String[] items = line.split(" ");
+                if (items[0].equals(number.toString())) {
+                    for (String item : items) {
+                        list.add(new Integer(item));
+                    }
+                    list.remove(0);
+                }
+            }
+            return list;
+
+        } catch (FileNotFoundException e) {
+            return null;
+        } catch (IOException e) {
+            return null;
+        }
     }
+
+
     public boolean savePrimeNumber(Long number, List<Long> primeNumbers) {
+
+        StringBuffer sb = new StringBuffer();
+        sb.append(number + " ");
+
+        primeNumbers.forEach(num -> {
+            sb.append(num + " ");
+        });
+
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter("db.txt", true));
+            bw.newLine();
+            bw.write(sb.toString());
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            return false;
+        }
         return true;
     }
 }
