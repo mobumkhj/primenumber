ca61a6b14e868ddf0f6f7c26307a3dec3f2cb43d
stellakang <kangsj1235@gmail.com>
2023-02-08 14:38:00 +0900

    Update Structure class: add getReversedPrimeList

--- src/Structure.java
+++ src/Structure.java
@@ -2,14 +2,24 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.Collections;
 
 public class Structure {
     public List<Long> getPrimeList(String data) {
-        // data form: "1.2.3.4.5"
+        // data form: "1|2|3|4|5"
         List<String> elements = Arrays.asList(data.split("\\|"));
         List<Long> numbers = new ArrayList<Long>();
         for (String str : elements) numbers.add(Long.valueOf(str));
         // return: List<Long> type (ex. [1,2,3,4,5])
+        return numbers;
+    }
+    public List<Long> getReversedPrimeList(String data) {
+        // data form: "1|2|3|4|5"
+        List<String> elements = Arrays.asList(data.split("\\|"));
+        List<Long> numbers = new ArrayList<Long>();
+        for (String str : elements) numbers.add(Long.valueOf(str));
+        Collections.reverse(numbers);
+        // return: List<Long> type (ex. [5,4,3,2,1])
         return numbers;
     }
     public String getPrimeString(List<Long> numbers){
@@ -17,7 +27,7 @@ public class Structure {
         String result = numbers.stream()
                 .map(n -> String.valueOf(n))
                 .collect(Collectors.joining("|"));
-        // return: String type (ex. "1.2.3.4.5")
+        // return: String type (ex. "1|2|3|4|5")
         return result;
     }
 }
