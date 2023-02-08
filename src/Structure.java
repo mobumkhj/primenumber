dea0b84363acb01e475202334c0cc03a288ad6ff
stellakang <kangsj1235@gmail.com>
2023-02-08 13:15:22 +0900

    Update Structure class: add getPrimeString

--- src/Structure.java
+++ src/Structure.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Structure {
     public List<Long> getPrimeList(String data) {
@@ -10,5 +11,13 @@ public class Structure {
         for (String str : elements) numbers.add(Long.valueOf(str));
         // return: List<Long> type (ex. [1,2,3,4,5])
         return numbers;
+    }
+    public String getPrimeString(List<Long> numbers){
+        // numbers: [1, 2, 3, 4, 5]
+        String result = numbers.stream()
+                .map(n -> String.valueOf(n))
+                .collect(Collectors.joining("."));
+        // return: String type (ex. "1.2.3.4.5")
+        return result;
     }
 }
