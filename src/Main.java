f02660cd8148032fc619db082f940665ec82195c
mobum <mobumkhj@gmail.com>
2023-02-08 11:57:56 +0900

    Add main implementation
    Signed-off-by: mobum <mobumkhj@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 입력하세요: ");
+        long input = sc.nextInt();
+        List<Integer> output = null;
+
+        DB l1 = new DB();
+        output = l1.getPrimeNumbers(input);
+
+        if(output == null){
+            PrimeNumber l3 = new PrimeNumber();
+            output = l3.makePrimeNumber(input);
+        }
+
+        System.out.println("소수: ");
+        System.out.println(output);
+        l1.savePrimeNumber(input, output);
     }
 }
