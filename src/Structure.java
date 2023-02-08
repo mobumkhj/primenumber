1b6da3a4df44396cd7133ff5f03ccc29cca47ab9
stellakang <kangsj1235@gmail.com>
2023-02-08 13:25:38 +0900

    Update Structure class: change delimiter

--- src/Structure.java
+++ src/Structure.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 public class Structure {
     public List<Long> getPrimeList(String data) {
         // data form: "1.2.3.4.5"
-        List<String> elements = Arrays.asList(data.split("\\."));
+        List<String> elements = Arrays.asList(data.split("\\|"));
         List<Long> numbers = new ArrayList<Long>();
         for (String str : elements) numbers.add(Long.valueOf(str));
         // return: List<Long> type (ex. [1,2,3,4,5])
@@ -16,7 +16,7 @@ public class Structure {
         // numbers: [1, 2, 3, 4, 5]
         String result = numbers.stream()
                 .map(n -> String.valueOf(n))
-                .collect(Collectors.joining("."));
+                .collect(Collectors.joining("|"));
         // return: String type (ex. "1.2.3.4.5")
         return result;
     }
