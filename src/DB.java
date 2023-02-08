3bdfb038c950b2d7f3a905ee8ce399ca971da735
mobum <mobumkhj@gmail.com>
2023-02-08 11:55:27 +0900

    Add DB class
    Signed-off-by: mobum <mobumkhj@gmail.com>

---
+++ src/DB.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class DB {
+    public List getPrimeNumbers(Long number) {
+        return null;
+    }
+    public boolean savePrimeNumber(Long number, List<Integer> primeNumbers) {
+        return true;
+    }
+}
